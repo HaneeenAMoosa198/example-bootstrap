2d7f5884125064d8103d48448f2a574c1c2b7123
haneen ahmad <youremail@example.com>
2023-08-02 21:29:41 +0300

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+// this is a java sctipt homework 
+
+
+// create an app.js that will have three different varaibales of string and print those variables in one 
+//line but in capital case and between each two variable there is the @ sybmobl 
+const myname ="HANEEN     "
+let mystudy ="  &  REACT     &     "
+let myage =25
+
+console.log((myname.concat(mystudy).concat(myage)))
